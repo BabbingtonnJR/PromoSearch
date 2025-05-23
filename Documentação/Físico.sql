a8893f3bfe215e23e73d0d1888d2a5e000f93f94
BabbingtonnJR <162913182+BabbingtonnJR@users.noreply.github.com>
2025-05-23 11:16:39 -0300

    Update Físico.sql

--- Documentação/Físico.sql
+++ Documentação/Físico.sql
@@ -5,6 +5,8 @@ CREATE TABLE Usuario(
 	id INT NOT NULL AUTO_INCREMENT,
     login VARCHAR(50) NOT NULL,
     senha VARCHAR(144) NOT NULL,
+	reset_token VARCHAR(64) NULL,
+	reset_expira DATETIME NULL,
     nome VARCHAR (100) NOT NULL,
     email VARCHAR(100) NOT NULL,
 	telefone VARCHAR(11) NOT NULL,
